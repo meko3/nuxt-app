e5c6e73bfd7ab8811538432731ff6d3f5d2207ee
meko3 <hirono.yoneyama@outlook.com>
2019-07-05 21:57:07 +0900

    [refactor] storybook to components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from "@storybook/vue";
 
-const req = require.context("../components", true, /.story.ts$/);
+const req = require.context("../components", true, /.stories(.js|.jsx|.ts)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
