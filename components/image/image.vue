30ba438132ba99663bea4a9f92d4823b13bddfb7
meko3 <hirono.yoneyama@outlook.com>
2019-07-06 19:36:01 +0900

    [feat] display image

---
+++ components/image/image.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="treasure" />
+  </div>
+</template>
+
+<style>
+.treasure {
+  height: 200px;
+  background: url(../../assets/treasure.svg) no-repeat;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <div>Home</div>
+    <Photo></Photo>
   </div>
 </template>
 
 <script>
+import Photo from "~/components/image/image.vue";
+
 export default {
+  components: {
+    Photo,
+  },
   layout: "base",
 }
 </script>
