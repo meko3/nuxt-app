ff16cc5d7ad6df3c6d33c95e22736a28e4328670
meko3 <hirono.yoneyama@outlook.com>
2019-07-06 23:27:06 +0900

    Updates

--- docs/_nuxt/c08081c0ad82c883f3a7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1106:function(t,e,n){"use strict";n.r(e);var l={layout:"base"},c=n(47),component=Object(c.a)(l,function(){this.$createElement;this._self._c;return this._m(0)},[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("div",[this._v("About")])])}],!1,null,null,null);e.default=component.exports}}]);
